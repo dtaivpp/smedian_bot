c6041ecd63d74dc3472ff4b81ff2c18523665fe8
David Tippett <David.Tippett@cbn.org>
2019-10-17 13:35:09 -0400

    Added initial files

---
+++ src/mongo_generators.py
@@ -0,0 +1,44 @@
+import pymongo
+
+def _get_all_generator(collection):
+  """
+  This returns a get function that gets all 
+    the documents from a collection
+  """
+
+  def get_all():
+    return collection.find({})
+
+  return get_all
+
+def _update_one_generator(collection):
+  """ 
+  Here we are creating mentods for updating one object in the database
+  """
+  def update_obj(obj):
+    """
+    This method takes in an obj with an id and will update
+      the object with the new data
+    """
+    if collection.count_documents({"_id": obj['_id']}) == 0:
+      collection.insert_one(obj)
+    else:
+      old = collection.find_one({'_id': obj['_id']})
+
+      updatedObj = _obj_dif_getter(old, obj) 
+      collection.update_one({'_id': obj['_id']}, {'$set': update_obj}) 
+
+
+def _obj_dif_getter(oldObj, newObj):
+  updatedObj = {}
+
+  for key in newObj.keys():
+    if key not in oldObj:
+      continue
+
+    if oldObj[key] == newObj[key]:
+      continue
+        
+    updatedObj[key] = newObj[key]
+  
+  return updatedObj
---
+++ src/scrape_smedian.py
@@ -0,0 +1,91 @@
+import requests
+from random import randint
+from time import sleep
+import json
+from copy import deepcopy
+
+# Request data
+def _request_data(url, retry=0):
+  response = requests.get(url)
+
+  if retry > 2:
+    with open('error.log', 'w') as f:
+      f.write("URL: {}\n Failed after 3 attemtps".format(url))
+
+    raise Exception('Too many failed attempts')
+
+  status = response.status_code
+
+  if status == 200 or status == 202:
+    data = response.json()  
+
+  elif status == 404:
+    _request_data(url, 3)
+
+  else:
+    sleep(10, 20)
+    _request_data(url, retry + 1)
+
+  return response.json()
+
+
+# Extract page data
+def extract_data(data):
+  last_pub_name, last_pub_id = None, None
+
+  url_list = [item['url'] for item in data["values"]]
+
+  if 'name' in data["paging"]["from"]:
+    last_pub_name = data["paging"]["from"]["name"]
+    last_pub_id = data["paging"]["from"]["_id"]
+
+  return last_pub_name, last_pub_id, url_list
+
+
+# Generate next url
+def _generate_next_url(PubName, PubId):
+
+  if PubName == None or PubId == None:
+    raise AttributeError("PubName and PubId must have values. \
+      PubName: {}, PubId: {}".format(PubName, PubId))
+
+  baseurl = 'https://www.smedian.com/api/i/pub/advertised' + \
+			'?bpa=true&limit=100&from=%7B%22name%22%3A%22'
+  
+  FormattedName = str(PubName).replace(" ", "%20")
+  
+  FormattedURL = "".join([baseurl, 
+						  FormattedName, 
+						  "%22%2C%22_id%22%3A%22", 
+						  PubId, 
+						  "%22%7D"])
+  
+  return FormattedURL
+
+
+def get_smedian_publications(seed_url):
+  """ 
+  Returns all smedian publications in a list
+
+  Input: Seed Url
+  """
+  
+  url = deepcopy(seed_url)
+  pub_list = []
+
+  while url != None:
+    data  = _request_data(url)
+    nextPubName, nextPubId, urlList = extract_data(data)
+    
+    pub_list = pub_list + urlList
+
+    if nextPubName != None:
+      url = _generate_next_url(nextPubName, nextPubId)
+    
+    else: url = None
+
+  return pub_list
+
+# Program flow
+if __name__ == '__main__':
+  pass
