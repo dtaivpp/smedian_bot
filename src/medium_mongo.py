acbbb6628ba09d790fad81c8f446fc77cd2674ee
David Tippett <David.Tippett@cbn.org>
2019-10-17 15:37:23 -0400

    Updated to use proper collections for gen function

--- src/medium_mongo.py
+++ src/medium_mongo.py
@@ -1,5 +1,8 @@
 from pymongo import MongoClient
-from mongo_generators import _get_all_generator, _update_one_generator, _exists_generator
+from mongo_generators import _get_all_generator, \
+                             _update_one_generator, \
+                             _exists_generator, \
+                             _delete_one_generator
 
 class Medium(object):
   def __init__(self):
@@ -13,17 +16,23 @@ class Medium(object):
     # Generate the getter for all the documents in file
     self.get_all_Publications = _get_all_generator(self.Publications)
     self.get_all_Posts = _get_all_generator(self.Posts)
-    self.get_all_Users = _get_all_generator(self.Publications)
-    self.get_all_Collections = _get_all_generator(self.Publications)
+    self.get_all_Users = _get_all_generator(self.Users)
+    self.get_all_Collections = _get_all_generator(self.Collections)
 
     # Generate the update_one for single document in the database
     self.update_one_Publication = _update_one_generator(self.Publications)
-    self.update_one_Post = _update_one_generator(self.Publications)
-    self.update_one_User = _update_one_generator(self.Publications)
-    self.update_one_Collection = _update_one_generator(self.Publications)
+    self.update_one_Post = _update_one_generator(self.Posts)
+    self.update_one_User = _update_one_generator(self.Users)
+    self.update_one_Collection = _update_one_generator(self.Collections)
 
     # Generate the check if exists funcitons
     self.exists_Publication = _exists_generator(self.Publications)
-    self.exists_Post = _exists_generator(self.Publications)
-    self.exists_User = _exists_generator(self.Publications)
-    self.exists_Collection = _exists_generator(self.Publications)
+    self.exists_Post = _exists_generator(self.Posts)
+    self.exists_User = _exists_generator(self.Users)
+    self.exists_Collection = _exists_generator(self.Collections)
+
+    # Generate delete functinon for objects
+    self.delete_one_Publication = _delete_one_generator(self.Publications)
+    self.delete_one_Post = _delete_one_generator(self.Posts)
+    self.delete_one_User = _delete_one_generator(self.Users)
+    self.delete_one_Collection = _delete_one_generator(self.Collections)
